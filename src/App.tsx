f4617726921a5ff537a7193e1af42361ed54e0df
omzkiii <geogeomarsantos@gmail.com>
2025-10-28 01:04:50 +0800

    set home structure

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,21 @@ import "./App.css";
 function App() {
   return (
     <>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
-      <div>Geomar Santos</div>
+      <div className="home">
+        <nav>
+          <a>About</a>
+          <a>Skills</a>
+          <a>Projects</a>
+        </nav>
+        <div>
+          <div id="center-container">
+            <div id="hello">hello,</div>
+            <div id="myNameIs">my name is</div>
+            <div id="geomarSantos">Geomar Santos</div>
+          </div>
+          Software Engineer
+        </div>
+      </div>
     </>
   );
 }
