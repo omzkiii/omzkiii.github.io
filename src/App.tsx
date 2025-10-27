a06e14fc4b343a80940d0c3d7856c5b8651c2eb3
omzkiii <geogeomarsantos@gmail.com>
2025-10-28 01:45:02 +0800

    add components and color

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,10 @@
 import "./App.css";
+import Home from "./components/Home";
 
 function App() {
   return (
     <>
-      <div className="home">
-        <nav>
-          <a>About</a>
-          <a>Skills</a>
-          <a>Projects</a>
-        </nav>
-        <div>
-          <div id="center-container">
-            <div id="hello">hello,</div>
-            <div id="myNameIs">my name is</div>
-            <div id="geomarSantos">Geomar Santos</div>
-          </div>
-          Software Engineer
-        </div>
-      </div>
+      <Home />
     </>
   );
 }
---
+++ src/components/Home.tsx
@@ -0,0 +1,27 @@
+import "./Home.css";
+
+function Home() {
+  return (
+    <>
+      <div id="home">
+        <div id="home-container">
+          <nav>
+            <a>About</a>
+            <a>Skills</a>
+            <a>Projects</a>
+          </nav>
+          <div>
+            <div id="center-container">
+              <div id="hello">hello,</div>
+              <div id="myNameIs">my name is</div>
+              <div id="geomarSantos">Geomar Santos</div>
+            </div>
+            Software Developer
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default Home;
