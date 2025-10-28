3483cc81759677fd0037874dd82ff3a94ca935f1
omzkiii <geogeomarsantos@gmail.com>
2025-10-28 17:27:19 +0800

    about page and reponsiveness

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import "./App.css";
+import About from "./components/About";
 import Home from "./components/Home";
 
 function App() {
   return (
     <>
       <Home />
+      <About />
     </>
   );
 }
---
+++ src/components/About.tsx
@@ -0,0 +1,42 @@
+import "./About.css";
+import portrait from "../assets/portrait.jpg";
+import github from "../assets/github.svg";
+import gmail from "../assets/gmail.svg";
+import linkedin from "../assets/linkedin.svg";
+
+function About() {
+  return (
+    <>
+      <div id="about">
+        <div id="intro">
+          <a>Hi, I'm Geo!</a>
+          <div id="red-box">
+            <div id="intro-box">
+              I work across the stack, from backend logic to user-facing design;
+              and build scalable, efficient systems that solve real-world
+              problems. Seeking software engineering opportunities to leverage
+              technical skills and innovative problem-solving abilities.
+            </div>
+          </div>
+        </div>
+        <img id="portrait" src={portrait} />
+        <div id="links">
+          <div>
+            <img src={github} />
+            <a>github.com/omzkiii</a>
+          </div>
+          <div>
+            <img src={linkedin} />
+            <a>geomar-santos-4056a7129</a>
+          </div>
+          <div>
+            <img src={gmail} />
+            <a>geogeomarsantos@gmail.com</a>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default About;
